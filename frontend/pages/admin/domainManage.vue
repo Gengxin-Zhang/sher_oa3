be304c7dbec046a1dd35484fb582a81223158bc1
wangsz12 <wangsz12@foxmail.com>
2020-08-03 22:11:05 +0800

    初始化项目

---
+++ frontend/pages/admin/domainManage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    name: 'domainManage',
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ frontend/pages/supervisor/groupManage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+  export default {
+    name: 'groupManage',
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
