5603c5f1ba7fb980cd3e1d475a5c1ba5140d62e6
wangsz12 <wangsz12@foxmail.com>
2020-08-04 22:47:45 +0800

    修改部分eslint规则

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -15,42 +15,44 @@ module.exports = {
   ],
   // add your custom rules here
   rules: {
-    "indent": ["off", 2],
-    "vue/max-attributes-per-line": [2, {
-      "singleline": 10,
-      "multiline": {
-        "max": 1,
-        "allowFirstLine": false
+    indent: ['off', 2],
+    'vue/max-attributes-per-line': [2, {
+      singleline: 10,
+      multiline: {
+        max: 1,
+        allowFirstLine: false
       }
     }],
-    "vue/html-self-closing": ["error", {
-      "html": {
-        "void": "any",
-        "normal": "any",
-        "component": "any"
+    'vue/html-self-closing': ['error', {
+      html: {
+        void: 'any',
+        normal: 'any',
+        component: 'any'
       },
-      "svg": "any",
-      "math": "any"
+      svg: 'any',
+      math: 'any'
     }],
-    "vue/singleline-html-element-content-newline": "off",
-    'vue/no-parsing-error': [2, { "invalid-first-character-of-tag-name": false }],
-    "vue/no-use-v-if-with-v-for": "off",
-    "vue/multiline-html-element-content-newline": "off",
-    "vue/name-property-casing": [0, "PascalCase"],
-    "vue/html-closing-bracket-newline": [0, {
-      "singleline": "never",
-      "multiline": "always"
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/no-parsing-error': [2, { 'invalid-first-character-of-tag-name': false }],
+    'vue/no-use-v-if-with-v-for': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/name-property-casing': [0, 'PascalCase'],
+    'vue/html-closing-bracket-newline': [0, {
+      singleline: 'never',
+      multiline: 'always'
     }],
-    "vue/no-side-effects-in-computed-properties": 0,
+    'vue/no-side-effects-in-computed-properties': 0,
     'arrow-spacing': [2, {
-      'before': true,
-      'after': true
+      before: true,
+      after: true
     }],
     'comma-dangle': [2, 'never'],
     'comma-spacing': [2, {
-      'before': false,
-      'after': true
+      before: false,
+      after: true
     }],
-    "space-before-function-paren": 0,
+    'space-before-function-paren': 0,
+    'brace-style': 0,
+    'no-console': 0
   }
 }
