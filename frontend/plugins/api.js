e5ffbac07f42935ee79b39a9a12bb43baace82ad
wangsz12 <wangsz12@foxmail.com>
2020-08-05 22:41:52 +0800

    增加basic的挂载

--- frontend/plugins/api.js
+++ frontend/plugins/api.js
@@ -1,9 +1,11 @@
 import Auth from '@/api/auth'
+import Basic from '@/api/basic'
 
 export default (context, inject) => {
   // Initialize API repositories
   const repositories = {
-    auth: Auth(context.$axios)
+    auth: Auth(context.$axios),
+    basic: Basic(context.$axios)
   }
   inject('api', repositories)
 }
