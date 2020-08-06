20027630563c69de19c43e055de5c0d7a817d7ce
wangsz12 <wangsz12@foxmail.com>
2020-08-06 22:54:14 +0800

    实现对签到值班等请求的封装

--- frontend/api/basic.js
+++ frontend/api/basic.js
@@ -1,5 +1,20 @@
 export default $axios => ({
   signin() {
     return $axios.get('/sign/do')
+  },
+  mySigns() {
+    return $axios.get('/sign/ls')
+  },
+  exportSigns() {
+    return $axios.get('/sign/export')
+  },
+  initRoutine(signtime) {
+    return $axios.post('/routine/init', signtime)
+  },
+  allRoutine() {
+    return $axios.get('/routine/ls')
+  },
+  myRoutine() {
+    return $axios.get('/routine/my')
   }
 })
