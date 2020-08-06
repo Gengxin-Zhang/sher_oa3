d1f6a09df04ff0cb7a22691e752df39411c82bd5
wangsz12 <wangsz12@foxmail.com>
2020-08-06 22:54:41 +0800

    实现本地存储用户信息

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <!-- dev test!! -->
+
+    <h1>Dev Pad</h1>
+    <h2>当前用户：&nbsp;{{ this.$store.state.user.userInfo.name }}</h2>
+
+    <!-- dev test!! -->
     <Nuxt />
   </div>
 </template>
@@ -14,21 +20,24 @@
       }
     },
     mounted() {
-      const logined = localStorage.getItem('logined') || false
+      const logined = localStorage.getItem('logined') === 'true' || false
       this.setLogined(logined)
 
       if (logined) {
         const token = localStorage.getItem('token') || undefined
         if (token === undefined) {
           this.$message.error('请先登录')
-          this.$router.push('auth/login')
+          this.$router.push('/auth/login')
         }
-        else if (!this.$api.auth.validateToken(token)) {
+        else if (!this.$api.auth.validateToken()) {
           this.$message.error('登录信息过期，请重新登录')
-          this.$router.push('auth/login')
+          this.$router.push('/auth/login')
         }
         else {
-          this.$router.push('/')
+          console.log('token有效')
+          const userInfo = JSON.parse(localStorage.getItem('userInfo'))
+          this.$store.commit('user/setUserInfo', userInfo)
+          this.setToken(token)
         }
       }
       else {
